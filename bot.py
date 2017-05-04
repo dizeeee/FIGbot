fa5255629488555671e52c9c42a3cb4f31132407
Jack <frog4511@gmail.com>
2017-05-04 18:35:03 -0500

    added !fig command and got it to output at working string

--- bot.py
+++ bot.py
@@ -1,4 +1,23 @@
-# just gonna get figlet working atm
+import discord
+from discord.ext import commands
 import subprocess
-result = subprocess.run(['figlet', 'a'], stdout=subprocess.PIPE)
-print(result.stdout.decode('utf-8'))
+
+def figlet(text):
+    result = subprocess.run(['figlet', text], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+token = input('What is your token?: ')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Connected!')
+    print('User: ' + bot.user.name)
+    print('ID: ' + bot.user.id)
+
+@bot.command(description='Prints ASCII text')
+async def fig(input):
+    await bot.say('```' + figlet(input) + '```')
+
+bot.run(token)
